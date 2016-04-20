66581a803e193aede511320afcb9ea99f000ea6c
Osman Ak <oaak001@gmail.com>
2016-04-20 11:52:44 -0500

    - Added log4j tool for compilation logging - Deleted Login Servlet, functionality added to Login Controller - Changed URL for dispatcher servlet

--- Osman-first-webapp/src/main/java/person/Osman/jee/LoginService.java
+++
@@ -1,8 +0,0 @@
-package person.Osman.jee;
-
-public class LoginService {
-	public boolean validateUser(String user, String password) {
-		return user.equalsIgnoreCase("Osman") && password.equals("password");
-	}
-
-}
