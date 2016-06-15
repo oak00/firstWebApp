e057cc6038bafa196a1a30a5f8f79a6a53fbb3da
Osman Ak <oaak001@gmail.com>
2016-06-14 21:50:32 -0500

    Expanded REST functionality - added path variable to be able to retrieve individual todos

--- Osman-first-webapp/src/main/java/com/in28minutes/todo/rest/ToDoRestController.java
+++ Osman-first-webapp/src/main/java/com/in28minutes/todo/rest/ToDoRestController.java
@@ -3,6 +3,7 @@ package com.in28minutes.todo.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +19,12 @@ public class ToDoRestController {
 	@RequestMapping(path="/todos")
 	public List<Todo> retrieveAllTodos(){
 		return service.retrieveTodos("in28Minutes");
+		
+	}
+	
+	@RequestMapping(path="/todos/{id}")
+	public Todo retrieveTodo(@PathVariable int id){
+		return service.retrieveTodo(id);
 		
 	}
 	
